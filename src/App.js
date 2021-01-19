864b99f80999ba0fd8b5a9da40505957febffccc
aquariax <roberthrrs8@gmail.com>
2021-01-18 20:01:31 -0500

    created reducer and implemented context api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
+import Player from './Player';
 import { getTokenFromUrl } from './spotify';
 import SpotifyWebApi from 'spotify-web-api-js';
 
